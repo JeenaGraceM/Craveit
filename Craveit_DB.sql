f0438490add9d0c251bfece45f5f7f47fdc64f56
JeenaGraceM <jeenagracem@gmail.com>
2025-01-25 20:24:59 +0530

    added two new databases- ingredients & recipe_ingredients

--- Craveit_DB.sql
+++ Craveit_DB.sql
@@ -1,10 +1,8 @@
 create database Craveit;
+
 use Craveit;
-create table Users(user_id int auto_increment primary key, user_name varchar(100) not null, email varchar(100) unique not null, password varchar(100) unique not null);
-alter table Users add DOB date not null;
-alter table Users add state varchar(100) not null;
-alter table Users add country varchar(100) not null;
-alter table Users add gender varchar(10) not null;
+
+create table Users(user_id int auto_increment primary key, user_name varchar(100) not null, email varchar(100) unique not null, password varchar(100) unique not null, DOB date not null, state varchar(100) not null, country varchar(100) not null, gender varchar(10) not null);
 desc Users;
 +-----------+--------------+------+-----+---------+----------------+
 | Field     | Type         | Null | Key | Default | Extra          |
@@ -35,3 +33,28 @@ desc recipes;
 +--------------+--------------+------+-----+---------+----------------+
 6 rows in set (0.00 sec)
 
+create table ingredients (ing_id int auto_increment primary key, ing_name varchar(100) not null, img_url varchar(255));
+Query OK, 0 rows affected (0.02 sec)
+
+ desc ingredients;
++----------+--------------+------+-----+---------+----------------+
+| Field    | Type         | Null | Key | Default | Extra          |
++----------+--------------+------+-----+---------+----------------+
+| ing_id   | int          | NO   | PRI | NULL    | auto_increment |
+| ing_name | varchar(100) | NO   |     | NULL    |                |
+| img_url  | varchar(255) | YES  |     | NULL    |                |
++----------+--------------+------+-----+---------+----------------+
+3 rows in set (0.00 sec)
+
+create table recipe_ingredients(recipe_id int not null, ing_id int not null, quantity varchar(50) not null, foreign key (recipe_id) references recipes(recipe_id), foreign key (ing_id) references ingredients(ing_id));
+Query OK, 0 rows affected (0.06 sec)
+
+desc recipe_ingredients;
++-----------+-------------+------+-----+---------+-------+
+| Field     | Type        | Null | Key | Default | Extra |
++-----------+-------------+------+-----+---------+-------+
+| recipe_id | int         | NO   | MUL | NULL    |       |
+| ing_id    | int         | NO   | MUL | NULL    |       |
+| quantity  | varchar(50) | NO   |     | NULL    |       |
++-----------+-------------+------+-----+---------+-------+
+3 rows in set (0.00 sec)
